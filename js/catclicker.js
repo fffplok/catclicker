c738d36903f5c54baf4898f4edf9454ca2fe30a3
fffplok <fffplok@gmail.com>
2015-06-18 16:06:59 -0500

    admin functionality implemented

--- js/catclicker.js
+++ js/catclicker.js
@@ -46,6 +46,7 @@
 	};
 
 	var model = {
+		adminMode:1,
 		numCats:5,
 		numCatsReady:0,
 		curCat:0,
@@ -81,6 +82,10 @@
 
 	var viewMenu = {
 		arrMenu: document.getElementsByTagName('li'),
+
+		changeName: function(name, ix) {
+			this.arrMenu[ix].textContent = name;
+		},
 
 		render: function() {
 			//also update menu to show which cat is selected
@@ -149,7 +154,103 @@
 		},
 	};
 
+	var viewForm = {
+		render: function() {
+			this.inputName.value = octopus.getCatName();
+			this.inputUrl.value = octopus.getCatUrl();
+			this.inputClicks.value = octopus.getCount();
+		},
+
+		getName:function() {
+			return this.inputName.value;
+		},
+
+		getUrl:function() {
+			return this.inputUrl.value;
+		},
+
+		getCount:function() {
+			//should have err checking here
+			return parseInt(this.inputClicks.value);
+		},
+
+		showForm: function(b) {
+			this.elForm.style.display = (b) ? 'block' : 'none';
+		},
+
+		enableSaveCancel: function(b) {
+			this.buttonSave.disabled = (b) ? 0 : 1;
+			this.buttonCancel.disabled = (b) ? 0 : 1;
+		},
+
+		init: function() {
+			this.containerAdmin = document.getElementById('containerAdmin');
+			this.elForm = this.containerAdmin.getElementsByTagName('form')[0];
+			this.buttonAdmin = document.getElementById('buttonAdmin');
+			this.buttonSave = document.getElementById('buttonSave');
+			this.buttonCancel = document.getElementById('buttonCancel');
+			this.inputName = document.getElementById('textName');
+			this.inputUrl = document.getElementById('textUrl');
+			this.inputClicks = document.getElementById('textClicks');
+
+			this.buttonAdmin.addEventListener('click', (function(style){
+				return function() {
+					octopus.toggleForm(style);
+				};
+			})(this.elForm.style));
+
+			this.buttonSave.addEventListener('click', octopus.saveForm);
+			this.buttonCancel.addEventListener('click', octopus.cancelForm);
+			this.enableSaveCancel(0);
+
+			this.containerAdmin.style.display = 'block';
+			this.render();
+			console.log('viewForm.init, viewForm:', this);
+		}
+	};
+
 	var octopus = {
+		keyHandler: document.addEventListener('keyup', function(e) {
+			if (e.target === viewForm.inputName || e.target === viewForm.inputUrl || e.target === viewForm.inputClicks) {
+				viewForm.enableSaveCancel(1);
+			}
+		}),
+
+		//style may be '', 'none', or 'block'
+		toggleForm: function(style) {
+			if (!style.display | style.display === 'none') {
+				viewForm.showForm(1);
+			} else {
+				viewForm.showForm(0);
+			}
+		},
+
+		enableFormButtons: function() {
+			console.log('enableFormButtons');
+		},
+
+		saveForm: function() {
+			var nam = viewForm.getName(),
+					imgUrl = viewForm.getUrl(),
+					count = viewForm.getCount();
+
+			//sync name
+			model.cats[model.curCat].name = nam;
+			viewMenu.changeName(nam, model.curCat);
+
+			//sync url, click count
+			model.cats[model.curCat].image.src = imgUrl;
+			model.cats[model.curCat].clickCount = count;
+
+			viewCat.render();
+			viewForm.enableSaveCancel(0);
+		},
+
+		cancelForm: function() {
+			viewForm.render();
+			viewForm.enableSaveCancel(0);
+		},
+
 		getCatSrc:function() {
 			return model.cats[model.curCat].image.src;
 		},
@@ -162,12 +263,17 @@
 			return model.cats[model.curCat].name;
 		},
 
+		getCatUrl:function() {
+			return model.cats[model.curCat].image.src;
+		},
+
 		getCatIx:function() {
 			return model.cats[model.curCat].ix;
 		},
 
 		incrementCount:function(e) {
 			viewCat.updateCount(model.incrementCount());
+			if (model.adminMode) viewForm.render(); //keep form in sync
 		},
 
 		getCount:function() {
@@ -178,6 +284,7 @@
 			model.curCat = i;
 			viewMenu.render();
 			viewCat.render();
+			if (model.adminMode) viewForm.render();
 		},
 
 		numCats:function() {
@@ -187,6 +294,7 @@
 		readyViews: function() {
 			viewMenu.init(model.numCats);
 			viewCat.init();
+			if (model.adminMode) viewForm.init(model.adminMode);
 		},
 
 		buildCats: function(arrNames) {
